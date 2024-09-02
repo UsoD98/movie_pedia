3912baa806bfcc1604e411db84698c916a8cc712
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-09-02 15:18:17 +0900

    DOCS: 초기 화면 진입 시 영화 페이지로 리다이렉트

--- src/main/java/com/pedia/movie/HomeController.java
+++ src/main/java/com/pedia/movie/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     @GetMapping("/")
     public String home()  {
 
-    return "home";
+    return "redirect:/films";
     }
     @GetMapping("/new")
     public String showNew() {
