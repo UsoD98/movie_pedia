6adbff7693f176905aca2e8279088f2dbcbba722
hamster0410 <gkstjsdn0011@naver.com>
2024-09-06 12:26:54 +0900

    Feat: 임시 비밀번호 메일 전송 & 비밀번호 수정
    임시 비밀번호를 메세지에 전달하고
    현재 비밀번호를 수정합니다.

--- src/main/java/com/pedia/movie/user/service/UserService.java
+++ src/main/java/com/pedia/movie/user/service/UserService.java
@@ -1,15 +1,22 @@
 package com.pedia.movie.user.service;
 
+import com.pedia.movie.config.MailConfig;
 import com.pedia.movie.user.dto.CommentResponse;
 import com.pedia.movie.user.dto.UserResponse;
 import com.pedia.movie.user.entity.Follow;
 import com.pedia.movie.user.entity.User;
 import com.pedia.movie.user.repository.FollowRepository;
 import com.pedia.movie.user.repository.UserRepository;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.SecureRandom;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +32,11 @@ public class UserService {
     private final UserRepository userRepository;
     private final FollowRepository followRepository;
     private final CommentService commentService;
+    private MailConfig mailConfig;
+
+    private final JavaMailSender javaMailSender; // JavaMailSender 인터페이스 사용
+
+
 
     // 회원가입
     @Transactional
@@ -75,6 +87,60 @@ public class UserService {
             return null;
         }
         return UserResponse.from(user);
+    }
+
+    //유저 정보 가져오기 네임 메일
+    @Transactional
+    public UserResponse findUserByNameAndEmail(String name, String email){
+        User user = userRepository.findByNameAndEmail(name,email);
+        if( user == null){
+            return null;
+        }
+        return UserResponse.from(user);
+    }
+
+    public String createNewPassword(){
+        System.out.println("createNewPassword()");
+        char[] chars = new char[] {
+                '0','1','2','3','4','5','6','7','8','9',
+                'a','b','c','d','e','f','g','h','i', 'j',
+                'k','l','m','n','o','p','q','r','s','t',
+                'u','v','w','x','y','z'
+        };
+
+        StringBuffer stringBuffer = new StringBuffer();
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.setSeed(new Date().getTime());
+
+        int index=0;
+        int length = chars.length;
+
+        for(int i = 0 ;i<8;i++){
+            index = secureRandom.nextInt(length);
+
+            if(index % 2 == 0)
+                stringBuffer.append(String.valueOf(chars[index]).toUpperCase());
+            else
+                stringBuffer.append(String.valueOf(chars[index]).toLowerCase());
+        }
+        System.out.println("NEW PASSWORD: " + stringBuffer.toString());
+
+        return stringBuffer.toString();
+    }
+
+    private void sendNewPasswordByMail(String toMailAddr, String newPassword){
+        System.out.println("sendNewPasswordByMail()");
+        final MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                final MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+                mimeMessageHelper.setTo(toMailAddr);
+                mimeMessageHelper.setSubject("왓챠피디아 새 비밀번호 안내입니다.");
+                mimeMessageHelper.setText("새 비밀번호: " + newPassword, true);
+            }
+        };
+        javaMailSender.send(mimeMessagePreparator); // JavaMailSender 사용
+
     }
 
     // 유저 팔로우
@@ -151,5 +217,37 @@ public class UserService {
     // 코멘트 확인
     public List<CommentResponse> getComments(Long userId) {
         return commentService.getComments(userId);
+    }
+
+    public int SetAndSendingPW( String email, String name) {
+
+        User user = userRepository.findByNameAndEmail(name,email);
+        if (user == null) {
+            return -2;
+        }
+
+        String tempPW = createNewPassword();
+        user.setPassword(tempPW);
+        userRepository.save(user);
+
+        sendNewPasswordByMail(email,tempPW);
+        return 1;
+    }
+
+    public int modifyPW(Long userId, String password, String newPassword, String newPasswordCheck) {
+        if(!newPassword.equals(newPasswordCheck)){
+            return -2;
+        }
+        User user = userRepository.findById(userId).orElse(null);
+        if(user == null){
+            return -2;
+        }
+        if(!password.equals(user.getPassword())){
+            return -1;
+        }
+        user.setPassword(newPassword);
+        userRepository.save(user);
+
+        return 1;
     }
 }
